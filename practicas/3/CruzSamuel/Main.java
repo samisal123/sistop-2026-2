7d95be508fb266da87a25c8bac85528b3032882c
Samuel Cruz <santi.sam120@gmail.com>
2026-02-24 22:32:50 -0600

    Sistema de escaneo de nombre de usuario agregado con éxito al hola mundo

--- practicas/3/CruzSamuel/Main.java
+++ practicas/3/CruzSamuel/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo pibes");
+        System.out.println("Hola Mundo pibe");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cómo te llamas pine");
+        String nombre_Usuario = sc.nextLine();
+        System.out.println("Hola " + nombre_Usuario + " Como estás pibe" );
     }
 }
