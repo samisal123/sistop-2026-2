82cbc362cf048196f35378843109772ac7f93424
David <david@pc-bd-dda.fi.unam>
2026-03-26 22:29:22 -0600

    implementacion de logica de run en alumno

--- tareas/2/ArzateAdrian-DiazDavid/alumnos_y_asesor.py
+++ tareas/2/ArzateAdrian-DiazDavid/alumnos_y_asesor.py
@@ -9,18 +9,33 @@ numero_sillas = 10
 
 
 class Alumno(th.Thread):
-	def __init__(self, id, semaforo, mutex):
+	def __init__(self, id, sillas, mutex):
 		super().__init__()
 		self.id = id
-		self.semaforo = semaforo
+		self.silla = sillas
 		self.mutex = mutex
 		self.numero_dudas = random.randint(1, 10)
 
 	def run(self):
-		semaforo.acquire()
-		
+		self.silla.acquire()
+		print(f"El alumno {self.id} entró al cubiculo")
 
-		semaforo.release()
+		self.resolver_dudas()
 
+		self.silla.release()
+		print(f"El alumno {self.id} salió del cubiculo")
 
+	def resolver_dudas(self):
+		while self.numero_dudas != 0:
+			
+			self.mutex.acquire()
+			
+			print(f"El alumno {self.id} está relaizando resolviendo una duda")
+			self.numero_dudas -= 1
+			
+			self.mutex.release()
+			
+			time.sleep(1)
+
+		print(f"El alumno {self.id} resolvio todas sus dudas") 
 
